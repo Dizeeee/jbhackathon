b6027c47e768f238f2204e5cdd4b103fd3e117c1
Jonathan Spikes <jmspikes@uark.edu>
2018-09-22 08:39:56 -0500

    Add files via upload

--- ducksandtrucks.js
+++ ducksandtrucks.js
@@ -34,6 +34,7 @@ function Model(){
 }
 
 Model.prototype.update = function(){
+	score +=1;
 	if(this.moveDir != null){
 		this.moveDuck();
 		this.moveDir = null;
@@ -45,6 +46,8 @@ Model.prototype.update = function(){
 			if(this.doICollide(step)){
 				x=10;
 				y=520;
+				lives -=1;
+				score -=50;
 			}
 				
 		}
@@ -63,12 +66,17 @@ Model.prototype.update = function(){
 		truck4 = new Image();
 		truck4.src = "hunts.png";
 		context.drawImage(def, 0, 0);
+		if(lives > 0)
 		context.drawImage(base, x, y);
+		else{
+			base.src="deadduck.png";
+			context.drawImage(base, x, y);
+		}
 		context.drawImage(truck1,tx1,ty1);
 		context.drawImage(truck2,tx2,ty2);
 		context.drawImage(truck3,tx3,ty3);
 		context.drawImage(truck4,tx4,ty4);
-			
+		
 
 }
 
@@ -228,9 +236,131 @@ function Game(){
 
 Game.prototype.onTimer = function(){
 	this.controller.update();
-	this.model.update();
+	if(lives > 0)
+		this.model.update();
 }
 
 let game = new Game();
 
 let timer = setInterval(function(){game.onTimer();},40);
+
+(function() {
+  // The width and height of the captured photo. We will set the
+  // width to the value defined here, but the height will be
+  // calculated based on the aspect ratio of the input stream.
+
+  var width = 320;    // We will scale the photo width to this
+  var height = 0;     // This will be computed based on the input stream
+
+  // |streaming| indicates whether or not we're currently streaming
+  // video from the camera. Obviously, we start at false.
+
+  var streaming = false;
+
+  // The various HTML elements we need to configure or control. These
+  // will be set by the startup() function.
+
+  var video = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+ function startup() {
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('startbutton');
+
+    navigator.getMedia = ( navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia);
+
+    navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
+
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+      
+        // Firefox currently has a bug where the height can't be read from
+        // the video, so we will make assumptions if this happens.
+      
+        if (isNaN(height)) {
+          height = width / (4/3);
+        }
+      
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    startbutton.addEventListener('click', function(ev){
+      takepicture();
+      ev.preventDefault();
+    }, false);
+    
+    clearphoto();
+  }
+
+  // Fill the photo with an indication that none has been
+  // captured.
+
+  function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+  
+  // Capture a photo by fetching the current contents of the video
+  // and drawing it into a canvas, then converting that to a PNG
+  // format data URL. By drawing it on an offscreen canvas and then
+  // drawing that to the screen, we can change its size and/or apply
+  // other changes before drawing it.
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+
+      imageData = context.getImageData(0, 0, 150,120);
+      colors = imageData.data;
+
+      for(var i = 0, n = colors.length; i < n; i++)
+      		colors[i] = 255-colors[i];
+      context.putImageData(imageData, 0, 0)
+    } else {
+      clearphoto();
+    }
+  }
+
+  // Set up our event listener to run the startup process
+  // once loading is complete.
+  window.addEventListener('load', startup, false);
+})();
